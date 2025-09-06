a861e2d3a1e56c9938a985a42e9dbf8702c05fe9
Aanya Sukhija <140511265+Aanyas24@users.noreply.github.com>
2025-09-06 21:51:51 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import React from "react";
- 
- 
-const App = () => {
-  return( 
-   <div>
-     
-   </div>
-  )
-};
+import Navbar from "./Navbar";
+import Header from "./Header";
+import Home from "./Home";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+}
 
 export default App;
