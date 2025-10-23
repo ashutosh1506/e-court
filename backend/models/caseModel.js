3ddacff3047b7685d964852fd8fa8bf4e0e130f3
Yuvan Grover <1095yuvangrover@gmail.com>
2025-10-24 00:23:36 +0530

    dataModelling for lawyer to check the case status done

---
+++ backend/models/caseModel.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+ 
+
+const caseSchema = new Schema({
+
+    caseType :{
+        type: String,
+        required: true,
+
+    },
+
+    caseNumber :{
+        type: Number,
+        required: true,
+        unique: true,
+    },
+
+    cnrNumber :{
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    nextHearingDate :{
+        type: Date,
+        required: true,
+    },
+    
+    status :{
+        type: String,
+    },
+
+    court :{
+        type: String
+    }
+
+},
+{
+    timestamps: true
+})
+
+
+export const caseModel = mongoose.model("Case", caseSchema);
