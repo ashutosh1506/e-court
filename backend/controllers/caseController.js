5b92086dcf657b569ac0b9bdaee44eaa27645c16
Yuvan Grover <1095yuvangrover@gmail.com>
2025-11-12 17:08:18 +0530

    removed seqarch by case type api

--- backend/controllers/caseController.js
+++ backend/controllers/caseController.js
@@ -1,35 +1,8 @@
-import { Case } from "../models/caseModel";
+import { Case } from "../models/caseModel.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 
-const searchByCaseType = asyncHandler(async (req, res) => {
-
-    const { caseType, caseNumber } = req.body
-
-    if (!caseType?.trim()) {
-        throw new ApiError(400, "Case type is required");
-    }
-
-    if (!caseNumber?.trim()) {
-        throw new ApiError(400, "Case number is required");
-    }
-
-    const caseData = await Case.findOne({
-        caseType: caseType.trim(),
-        caseNumber: caseNumber.trim()
-    })
-
-    if (!caseData) {
-        throw new ApiError(404, "Case not found with the provided details ");
-    }
-
-    return res
-        .status(200)
-        .json(
-            new ApiResponse(200, caseData, "Case found successfully")
-        );
-})
 
 const searchByCnrNumber = asyncHandler(async (req, res) => {
 
@@ -65,7 +38,5 @@ const searchByCnrNumber = asyncHandler(async (req, res) => {
 })
 
 export {
-
-    searchByCaseType,
     searchByCnrNumber
 };
