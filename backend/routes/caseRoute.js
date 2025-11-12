a9cf21439aeda999f904b80c4d42e6177c999904
Yuvan Grover <1095yuvangrover@gmail.com>
2025-11-12 17:36:09 +0530

    caseDeatils frontend part completed

--- backend/routes/caseRoute.js
+++ backend/routes/caseRoute.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/authMiddleware.js";
-import { searchByCaseType, searchByCnrNumber } from "../controllers/caseController.js";
+import {searchByCnrNumber } from "../controllers/caseController.js";
 import { createCase } from "../controllers/caseController.js";
 
 const router = Router();
 
 router.route("/searchByCnrNumber").post(verifyJWT, searchByCnrNumber);
-router.route("/searchByCaseType").post(verifyJWT, searchByCaseType);
-
 router.route("/registerCase").post(verifyJWT, createCase);
 
 export default router;
